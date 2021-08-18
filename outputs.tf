b195f786abb35873ec33c53c1b480a5253daeb80
deeliciouscode <schmiderdavid@gmail.com>
2021-08-18 16:52:27 +0200

    added outputs and fixed issue there

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ecs_cluster" {
-  value = aws_ecs_cluster.dev_to
+  value = aws_ecs_cluster.ecs_cluster
 }
 
 output "ecs_service" {
-  value = aws_ecs_service.dev_to
+  value = aws_ecs_service.ecs_service
 }
